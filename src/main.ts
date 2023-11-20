ebd2829cc9d3df9901adc001086c3c946ed6b7eb
Yari Herrera <yari@primeinsights.com>
2023-11-20 19:41:00 +0100

    Add pinia dependency for state management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 import router from './router';
 
+const pinia = createPinia();
 const forumApp = createApp(App);
+
+forumApp.use(pinia);
 forumApp.use(router);
 forumApp.mount('#app');
