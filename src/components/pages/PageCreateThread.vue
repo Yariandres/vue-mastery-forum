da92afeaa4735b893be0c7be0e3469c6a2c1dc7c
Yari Herrera <yari@primeinsights.com>
2023-11-25 00:05:19 +0100

    Add PageCreateThread component

---
+++ src/components/pages/PageCreateThread.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+  forum: any;
+}>();
+
+const title = ref('');
+const text = ref('');
+
+const save = () => {
+  console.log(title.value, text.value);
+  // pinia action
+};
+</script>
+<template>
+  <div class="col-full push-top">
+    <h1>
+      Create new thread in <i>{{ forum.name }}</i>
+    </h1>
+
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title</label>
+        <input
+          v-model="title"
+          type="text"
+          id="thread_title"
+          class="form-input"
+          name="title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="thread_content">Content</label>
+        <textarea
+          v-model="text"
+          type="text"
+          id="thread_content"
+          class="form-input"
+          name="content"
+        />
+      </div>
+      <div class="form-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button type="submit" class="btn btn-blue">Publish</button>
+      </div>
+    </form>
+  </div>
+</template>
